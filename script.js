151a76aa9df121960ff88158542ff51a4cc14844
Medido1 <medi1222@gmail.com>
2023-02-05 21:52:52 +0100

    add function to color grid in black with hover effect

--- script.js
+++ script.js
@@ -2,13 +2,23 @@ const grid =  document.getElementById("grid_container");
 grid.style.width = "396px";
 grid.style.height = "396px";
 
+let color = null;
+
 function buildGrid(size = 8){
   for (let i = 1; i <= size * size; i++){
     const box = document.createElement("div");
     box.style.width = (parseInt(grid.style.width) / size - 2) + "px";
     box.style.height = (parseInt(grid.style.height) / size -2) + "px";
     grid.appendChild(box);
+    box.addEventListener("mouseover", colorGrid);
   }
 }
 
 buildGrid();
+
+function colorGrid(color){
+  switch(color){
+    default: 
+      this.style.backgroundColor = "black";
+  }
+}
