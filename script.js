535597bc5b0aadfc96071f373e41270a6bcbb55a
Medido1 <medi1222@gmail.com>
2023-02-05 21:47:57 +0100

    add javascript function to create the grid with apporpriate css

--- script.js
+++ script.js
@@ -2,4 +2,13 @@ const grid =  document.getElementById("grid_container");
 grid.style.width = "396px";
 grid.style.height = "396px";
 
+function buildGrid(size = 8){
+  for (let i = 1; i <= size * size; i++){
+    const box = document.createElement("div");
+    box.style.width = (parseInt(grid.style.width) / size - 2) + "px";
+    box.style.height = (parseInt(grid.style.height) / size -2) + "px";
+    grid.appendChild(box);
+  }
+}
 
+buildGrid();
