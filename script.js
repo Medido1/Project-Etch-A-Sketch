1ce45100e377f1d0517e4ff9e1ebe9ef924f003f
Medido1 <medi1222@gmail.com>
2023-02-05 22:09:02 +0100

    create interactive button to choose the grid size

--- script.js
+++ script.js
@@ -4,6 +4,28 @@ grid.style.height = "396px";
 
 let color = null;
 
+const grid_btn = document.getElementById("grid_btn");
+
+grid_btn.addEventListener("click", ()=>{
+  clearGrid();
+  const size = parseInt(prompt("choose a grid size 1-100"));
+  if (size === 666){
+    alert(`The devil is not available at the moment
+    please try again later`);
+    return;
+  }
+  if (size > 100 || size < 0){
+    alert("Please choose a size between 1-100!!");
+    return;
+  }
+  else if (isNaN(size)){
+    alert("Please enter a valid number")
+    return;
+  }
+  
+  buildGrid(size);
+})
+
 function buildGrid(size = 8){
   for (let i = 1; i <= size * size; i++){
     const box = document.createElement("div");
@@ -21,4 +43,8 @@ function colorGrid(color){
     default: 
       this.style.backgroundColor = "black";
   }
+}
+
+function clearGrid(){
+  grid.innerHTML = "";
 }
